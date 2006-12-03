8be6ccd4c5f95a51c05f78cc22bb820d0b0b9ed3
rocky <rocky>
2006-12-03 03:14:54 +0000

    Add pathname_isofy and remove refereneces to psz.

--- iso9660.py
+++ iso9660.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-#    $Id: iso9660.py,v 1.9 2006/11/27 22:19:56 rocky Exp $
+#    $Id: iso9660.py,v 1.10 2006/12/03 03:14:54 rocky Exp $
 #
 #    Copyright (C) 2006 Rocky Bernstein <rocky@gnu.org>
 #
@@ -102,6 +102,16 @@ portion and the filename should be composed of only DCHARs.
   
 True is returned if path is valid."""
     return pyiso9660.pathame_valid(path)
+
+
+def pathname_isofy(path, version=1)
+"""pathname_valid_p(path, version=1)->string
+
+Take path and a version number and turn that into a ISO-9660 pathname.
+(That's just the pathname followed by ';' and the version number. For
+example, mydir/file.ext -> MYDIR/FILE.EXT;1 for version 1. The
+resulting ISO-9660 pathname is returned."""
+    return pyiso9660.pathname_isofy(path, version)
 
 def name_translate(filename, joliet_level=0):
     """name_translate(name, joliet_level=0)->str
@@ -380,7 +390,7 @@ class ISO9660:
         def stat(self, path, translate=False):
             """stat(self, path, translate=False)->{stat}
             
-            Return file status for path name psz_path. None is returned on
+            Return file status for path name path. None is returned on
             error.  If translate is True, version numbers in the ISO 9660
             name are dropped, i.e. ;1 is removed and if level 1 ISO-9660
             names are lowercased.
@@ -473,7 +483,7 @@ class ISO9660:
         def stat(self, path, translate=False):
             """stat(self, path, translate=False)->{stat}
             
-            Return file status for path name psz_path. None is returned on
+            Return file status for path name path. None is returned on
             error.  If translate is True, version numbers in the ISO 9660
             name are dropped, i.e. ;1 is removed and if level 1 ISO-9660
             names are lowercased.
